3f1cd35ac440506c769cc01d756395ef66b7fa06
Chad Ingram <Chad117gram@gmail.com>
2026-04-24 22:06:54 -0700

    Initial Commit

---
+++ FlightTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlightTracker
+//
+//  Created by chad ingram on 4/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlightTracker/FlightTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightTrackerApp.swift
+//  FlightTracker
+//
+//  Created by chad ingram on 4/24/26.
+//
+
+import SwiftUI
+
+@main
+struct FlightTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
